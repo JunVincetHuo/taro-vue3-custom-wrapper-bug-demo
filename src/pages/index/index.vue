5ab91f3b20e2a5dcabbed821788b1e81e2243039
junvincet <junvincethuo@outlook.com>
2023-09-26 10:37:01 +0800

    fist commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <custom-wrapper class="index">
+    <view>
+      <text>{{ msg }}</text>
+    </view>
+  </custom-wrapper>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.less'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
